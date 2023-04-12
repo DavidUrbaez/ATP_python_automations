5bb745aa865736b77d48780eb7631635dbbc0a77
DavidUrbaez <63584647+DavidUrbaez@users.noreply.github.com>
2023-04-12 11:45:00 -0500

    Create a BAT file to run ATP simulations
    For some reason I needed to move the working directory to "C:\ATP\atpmingw"
    That is why I have the following lines:
    
        C:
        CD C:\ATP\atpmingw
    
    Note: the symbol "::" means comment in bat files
    
    Then after changing the working directory I can run the line:
    
        C:\ATP\atpmingw\tpbig.exe disk C:\Users\USUARIO\Documents\ATPdata\work\system_110kv.atp s -r
    
    Where "C:\Users\USUARIO\Documents\ATPdata\work\system_110kv.atp" is the location of the ATP file

---
+++ RunATPSimulations/run_ATP.bat
@@ -0,0 +1,17 @@
+@echo off
+:: ####################################################
+:: Move to ATPMINGW folder
+:: ####################################################
+
+C:
+CD C:\ATP\atpmingw
+
+:: ####################################################
+:: Set variables
+:: ####################################################
+
+
+C:\ATP\atpmingw\tpbig.exe disk C:\Users\USUARIO\Documents\ATPdata\work\system_110kv.atp s -r
+
+
+pause
