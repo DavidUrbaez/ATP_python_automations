3b3f55d41d705bf8eb1b50dc950a9d266624c382
DavidUrbaez <63584647+DavidUrbaez@users.noreply.github.com>
2023-04-13 17:39:52 -0500

    Add time variable in the creation of the Database

--- RunATPSimulations/create_database.py
+++ RunATPSimulations/create_database.py
@@ -12,17 +12,26 @@ atp_template_file = TEMPLATES_FOLDER_PATH / "System_110kV_ThreePhase_Ground.atp"
 
 # define output folder/file
 OUTPUT_FOLDER = Path(r"H:/datasets/Simulations")
-OUTPUT_FOLDER_2 = Path(r"H:/datasets/NumpyResults")
-resistances = np.linspace(10, 1000, 100)
+NP_OUTPUT_FOLDER = Path(r"H:/datasets/NumpyResults")
+
+fault_resistances = np.linspace(100, 1000, 10)
+fault_times = np.linspace(0.02, 0.08, 6 * 10 + 1)
+
 # TODO: Use multiprocessing (or multithreading) library to parallelize this process
-for resistance in resistances:
-    resistance = np.round(resistance, 2)
-    atp_new_file = OUTPUT_FOLDER / ("3fg_r_" + str(resistance) + ".atp")
-    flag_key_R = "$FLAG_F_R$"
-    flag_value_R = f"{resistance:010.2f}"
 
-    flags = {flag_key_R: flag_value_R}
+for fault_resistance in fault_resistances:
+    for fault_time in fault_times:
 
-    create_atp_file(atp_template_file, flags, atp_new_file)
-    pl4_results = run_atp_simulation(atp_new_file)
-    pl4_to_npy(pl4_results, OUTPUT_FOLDER_2)
+        atp_new_file = OUTPUT_FOLDER / ("3fg_r_" + f"{fault_resistance:08.2f}" + "_t_" + f"{fault_time:05.5f}" + ".atp")
+
+        flag_key_T = "$FLAG_F_R$"
+        flag_value_T = f"{fault_resistance:010.2f}"
+
+        flags = {
+            "$FLAG_F_R$": f"{fault_resistance:010.2f}",
+            "$FLAG_F_T$": f"{fault_time:010.5f}",
+        }
+
+        create_atp_file(atp_template_file, flags, atp_new_file)
+        pl4_results = run_atp_simulation(atp_new_file)
+        pl4_to_npy(pl4_results, NP_OUTPUT_FOLDER)
