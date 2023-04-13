a80b8c6f3a3f89b51914143a61936fec9ca30211
DavidUrbaez <63584647+DavidUrbaez@users.noreply.github.com>
2023-04-13 10:43:53 -0500

    Refractor some variable in the main run ATP simulations file

--- RunATPSimulations/1_run_ATP_Simulations.py
+++ RunATPSimulations/1_run_ATP_Simulations.py
@@ -1,33 +1,32 @@
 from pathlib import Path
 import os
-
 import numpy as np
-
 from utils import readPL4
 
 
-def create_atp_file(template_file, flags, output_file):
+def create_atp_file(template_atp_file, dict_flags, new_atp_file):
     """
     Function that copies and pastes a template ATP file, then it replaces Flag values in the new
     ATP file.
-    :param template_file:
-    :param output_file:
+    :param dict_flags: 
+    :param template_atp_file:
+    :param new_atp_file:
     :return:
     """
     # Read ATP template file - Note: atp files could be interpreted as txt files
     # hence the variable "data" is going to be a large string variable.
-    with open(template_file, "r") as atp_template_file:
-        data = atp_template_file.read()
+    with open(template_atp_file, "r") as template_atp:
+        data = template_atp.read()
 
     # Since the variable "data" is a string, it is possible to replace the flags
     # with the corresponding values according to the "flags" dictionary
-    for flag, value in flags.items():
+    for flag, value in dict_flags.items():
         assert len(flag) == len(value), "Check the len of the flag and the values, they must be equal"
         data = data.replace(flag, value)
 
     # Finally save the "data" variable in the output file
-    with open(output_file, 'w') as atp_output_file:
-        atp_output_file.write(data)
+    with open(new_atp_file, 'w') as atp_file:
+        atp_file.write(data)
 
 
 def run_atp_simulation(atp_file):
@@ -37,7 +36,7 @@ def run_atp_simulation(atp_file):
     :return:
     """
     os.system(r"run_ATP.bat " + str(atp_file.resolve()))
-    pl4_results = Path(str(output_file.resolve()).replace("atp", "pl4"))
+    pl4_results = Path(str(atp_new_file.resolve()).replace("atp", "pl4"))
     return pl4_results
 
 
@@ -47,17 +46,17 @@ def pl4_to_npy(pl4_results):
 
     df_REG1 = df[df['FROM'].str.contains("REG1")]
     output_array = data[:, df_REG1.index]
-    np.save(str(output_file.resolve()).replace(".atp", '.npy'), output_array)
+    np.save(str(atp_new_file.resolve()).replace(".atp", '.npy'), output_array)
     # np.savetxt(str(output_file.resolve()).replace(".atp", '.csv'), output_array, delimiter=",")
 
 
 # Define template file
 TEMPLATES_FOLDER_PATH = Path(r"ATP_templates")
-template_file = TEMPLATES_FOLDER_PATH / "System_110kV_ThreePhase_Ground.atp"
+atp_template_file = TEMPLATES_FOLDER_PATH / "System_110kV_ThreePhase_Ground.atp"
 
 # define output folder/file
 OUTPUT_FOLDER = Path(r"H:\test_dataset")
-output_file = OUTPUT_FOLDER / "test0.atp"
+atp_new_file = OUTPUT_FOLDER / "test0.atp"
 
 R = 110
 flag_key_R = "$FLAG_F_R$"
@@ -66,6 +65,6 @@ flag_value_R = f"{R:010.2f}"
 flags = {flag_key_R: flag_value_R}
 
 if __name__ == '__main__':
-    create_atp_file(template_file, flags, output_file)
-    pl4_results = run_atp_simulation(output_file)
+    create_atp_file(atp_template_file, flags, atp_new_file)
+    pl4_results = run_atp_simulation(atp_new_file)
     pl4_to_npy(pl4_results)
