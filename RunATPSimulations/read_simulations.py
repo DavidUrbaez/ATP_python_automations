90638bc51d5460c7ab45a29dde12fb889c3765e6
DavidUrbaez <63584647+DavidUrbaez@users.noreply.github.com>
2023-04-17 15:12:14 -0500

    Encapsulate functionalities

--- RunATPSimulations/read_simulations.py
+++ RunATPSimulations/read_simulations.py
@@ -2,23 +2,45 @@ from pathlib import Path
 import h5py
 import numpy as np
 
-NP_OUTPUT_FOLDER = Path(r"H:/datasets/NumpyResults")
-full_data = []
-t = np.load(r"H:/datasets/NumpyResults/_time.npy")
 
-files = list(NP_OUTPUT_FOLDER.glob("[!_]*.npy"))
-files_len = len(files)
+def compress_data(numpy_outputs_folder_name: str | Path,
+                  output_file_name: str | Path,
+                  time_file_name: str = "_time.npy"):
+    """
+    Function that takes the numpy results folder and returns the h5 compressed file
 
-for index, file in enumerate(files):
+    :param time_file_name:
+    :param numpy_outputs_folder_name:
+    :param output_file_name:
+    :return:
+    """
+    full_data = []
+    numpy_outputs_folder = Path(numpy_outputs_folder_name)
+    result_files = list(numpy_outputs_folder.glob("[!_]*.npy"))
+    files_len = len(result_files)
 
-    if index % 100 == 0:
-        print(str(index) + "/" + str(files_len))
+    time_file_name = Path.joinpath(numpy_outputs_folder, time_file_name)
+    t = np.load(str(time_file_name))
+    for index, result_file in enumerate(result_files):
 
-    full_data.append(np.load(file))
+        if index % 100 == 0:
+            print(str(index) + "/" + str(files_len))
+        data = np.load(str(result_file))
+        full_data.append(data[::100, :])
 
-with h5py.File('H:/datasets/datos_fallas.h5', 'w') as hf:
-    hf.create_dataset('time', data=t)
+    with h5py.File(output_file_name, 'w') as hf:
+        hf.create_dataset('time', data=t)
 
-    hf.create_dataset('data', data=full_data)
+        hf.create_dataset('data', data=full_data)
 
-print("en voila!")
+    print("en voila!")
+
+
+if __name__ == "__main__":
+    numpy_results_folder = r"H:/datasets/1fg_NumpyResults"
+    output_file = "H:/datasets/datos_fallas.h5"
+
+    compress_data(
+        numpy_outputs_folder_name=numpy_results_folder,
+        output_file_name=output_file
+    )
